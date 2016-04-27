9130ff3b7ec3580084482eeda6291345984d88b8
ALinaDubatovka <alina.dubatovka@gmail.com>
2016-04-27 21:06:42 +0300

    added MendeleyApi

---
+++ src/com/bardsoftware/server/auth/provider/MendeleyApi.java
@@ -0,0 +1,53 @@
+/*
+Copyright (C) 2016 BarD Software
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.bardsoftware.server.auth.provider;
+
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.extractors.AccessTokenExtractor;
+import org.scribe.model.OAuthConfig;
+import org.scribe.utils.OAuthEncoder;
+import org.scribe.utils.Preconditions;
+
+/**
+ * Provides OAuth 2.0 configuration for mendeley.com
+ *
+ * @author alina.dubatovka@gmail.com
+ */
+public class MendeleyApi extends DefaultApi20 {
+  private static final String AUTHORIZE_URL = "https://api.mendeley.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code";
+  private static final String SCOPED_AUTHORIZE_URL = String.format("%s&scope=%%s", AUTHORIZE_URL);
+
+  @Override
+  public String getAccessTokenEndpoint() {
+    return "https://api.mendeley.com/access_token";
+  }
+
+  @Override
+  public String getAuthorizationUrl(OAuthConfig config) {
+    Preconditions.checkValidUrl(config.getCallback(), "Valid url is required for a callback. Mendeley does not support OOB");
+    if (config.hasScope()) {
+      return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()),
+          OAuthEncoder.encode(config.getScope()));
+    } else {
+      return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+    }
+  }
+
+  @Override
+  public AccessTokenExtractor getAccessTokenExtractor() {
+    return new JsonAccessTokenExtractor();
+  }
+}
